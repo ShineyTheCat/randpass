e6fdfaba40f28d87448f77638bf5e18cce0e2977
ShineyTheCat <149241963+ShineyTheCat@users.noreply.github.com>
2023-11-02 17:51:37 +0300

    shakal

---
+++ lesson_2.py
@@ -0,0 +1,10 @@
+from random import choice
+
+symbol = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+lenght_pass = int(input("Введите длину вашего пароля: "))
+shakal = ""
+
+for i in range(lenght_pass):
+    shakal += choice(symbol)
+
+print("Ваш пароль:", shakal)
